265d1e1af2218d0ab0e5c74ba868c256a7de0b5a
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-12-10 00:09:38 -0800

    feat: configure toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from 'react-hot-toast';
+
 import './App.css';
 
 function App() {
   return (
     <>
       <div className="text-2xl font-bold text-amber-200">Hello world</div>
+      <Toaster position="top-center" />
     </>
   );
 }
