0fcb2713c375e6e9c1d5318b45aeb0a19df544c8
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-12-11 05:10:43 -0800

    feat: create atom components

---
+++ src/components/atoms/Checkbox.tsx
@@ -0,0 +1,28 @@
+import { InputHTMLAttributes } from 'react';
+
+export interface CheckboxProps extends Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'type'
+> {
+  label?: string;
+}
+
+export const Checkbox = ({
+  label,
+  className = '',
+  ...props
+}: CheckboxProps) => (
+  <label className="flex items-start gap-2 cursor-pointer group">
+    <input
+      type="checkbox"
+      className={`mt-1 w-4 h-4 text-cyan-600 border-gray-300 rounded 
+          focus:ring-cyan-500 focus:ring-2 cursor-pointer ${className}`}
+      {...props}
+    />
+    {label && (
+      <span className="text-sm text-gray-600 group-hover:text-gray-800 select-none">
+        {label}
+      </span>
+    )}
+  </label>
+);
